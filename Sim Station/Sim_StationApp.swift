83171ef6e1f2aa549d90af9b61ff256d8479332c
John Demirci <ucjdemirci@gmail.com>
2025-09-09 10:42:13 -0700

    Initial Commit

---
+++ Sim Station/Sim_StationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sim_StationApp.swift
+//  Sim Station
+//
+//  Created by John Demirci on 9/9/25.
+//
+
+import SwiftUI
+
+@main
+struct Sim_StationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
